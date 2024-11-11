476ee976e8af3a421bce3a7f90c893d0042a53b0
sangmin <min62645@naver.com>
2024-11-12 00:14:29 +0900

    자바 프로그래밍 자습

--- chap_05/_03_MultiArray.java
+++ chap_05/_03_MultiArray.java
@@ -25,5 +25,19 @@ public class _03_MultiArray {
 
         //c5 에 접근하려면?
         System.out.println(seats [2] [4]);
+
+        //첫 줄에는 3칸, 둘쨰 줄에는 4칸, 셋째 줄에는 5칸
+        String [] [] seats2 = {
+                {"A1","A2","A3"},
+                {"B1","B2","B3","B4"},
+                {"C1","C2","C3","C4","C5"}
+        };
+
+        //A3에 접근 하려면?
+        System.out.println(seats[0][2]);
+
+        System.out.println("=====================");
+
+
     }
 }
