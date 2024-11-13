70e9fb6bc95cb7590cda5cd877878834e9e1c0ef
sangmin <min62645@naver.com>
2024-11-13 23:29:30 +0900

    자바 프로그래밍 자습

---
+++ chap_01/_03_Variables.java
@@ -0,0 +1,26 @@
+package chap_01;
+
+public class _03_Variables {
+    public static void main(String[] args) {
+        String name = "김상민";
+        int hour = 12;
+
+        System.out.println(name + "님, 배송이 시작되었습니다." + hour + "시에 방문 예정입니다.");
+        System.out.println(name + "님, 배송이 완료되었습니다.");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- chap_04/Test.java
+++ chap_04/Test.java
@@ -1,18 +1,23 @@
 package chap_04;
 
 public class Test {
-    public static void main(String[] args) {
-        int hour = 5;
-        boolean isSmallCar = false;
-        boolean withDisabledPerson = false;
+    public static String getHiddenData(String data, int number) {
+        String hiddenData = data.substring(0,number);
+        for (int i = 0; i < data.length() - number; i++) {
+            hiddenData += "*";
+        }
+        
+        return hiddenData;
+    }
 
-        int fee = hour * 4000;
-        if (fee > 30000) {
-            fee = 30000;
-        }
-        if (isSmallCar || withDisabledPerson) {
-            fee /= 2;
-        }
-        System.out.println("주차 요금은" + fee + "입니다.");
+
+    public static void main(String[] args) {
+        String name = "나코딩";
+        String id = "010626-3842711";
+        String phone = "010-7626-6672";
+
+        System.out.println("이름 :" + getHiddenData(name, 1));
+        System.out.println("주민등록번호 :" + getHiddenData(id, 8));
+        System.out.println("전화번호 :" + getHiddenData(phone, 9));
     }
 }
